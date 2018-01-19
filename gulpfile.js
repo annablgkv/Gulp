304f54b4e94a4e58bddb6069ea58811a43504723
annablgkv <annablgkv@gmail.com>
2018-01-19 23:41:21 +0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+		browserSync  = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {baseDir: './dev'},
+		notify: false
+	});
+});
+
+gulp.task('default', gulp.series('browser-sync'), function(){
+	gulp.watch('./dev/css/*.css', browserSync.reload);
+	gulp.watch('./dev/*.html', browserSync.reload);
+	gulp.watch('./dev/js/**/*.js', browserSync.reload);
+});
