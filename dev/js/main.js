af90347242f6e61b6cf870a106ea02f0ca3991b0
mioe <mioe.boss@gmail.com>
2018-02-03 05:56:14 +0300

    nav fixed and add nav logic

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,31 @@
+// nav logic (start)
+// --- animation (start)
+$(window).scroll(function () {
+  console.log($(window).scrollTop()) // проверка работы скролла
+  if ($(window).scrollTop() > 20) {
+    $('nav.navbar.navbar-toggleable-md.navbar-light').addClass('fixed-top')
+    $('nav').css({
+      "height": "75px",
+      "box-shadow": "2px 2px 24px rgba(0,0,0,0.1)",
+      "background": "rgb(246,246,246)",
+    })
+    $('#enterTop').css({
+      "margin-top": "75px",
+    })
+  } else {
+    $('nav.navbar.navbar-toggleable-md.navbar-light').removeClass('fixed-top')
+    $('nav').css({
+      "height": "116px",
+      "box-shadow": "none",
+      "background": "rgb(255,255,255)",      
+    })
+    $('#enterTop').css({
+      "margin-top": "0px",
+    })
+  }
+})
+// --- animation (end)
+// nav logic (end)
 
 // slider logic (start)
 $('#carouselExampleIndicators').mouseover(function () {
